4e5c3245b5699c99447e97f24bba56bc887d7e0a
Temesgen <tememuruts@gmail.com>
2023-06-14 12:42:54 +0800

    Fix: changed [nextauth].ts
    the file is configure to allow
    any user to have access to chatwindow
    with user:john@gmail.com pass:12345678

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,12 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
@@ -11,6 +15,31 @@ export const authOptions: NextAuthOptions = {
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    CredentialsProvider({
+      type: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "biglaw@email.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      authorize(credentials, req) {
+        const { email, password, name } = credentials as {
+          email: string;
+          password: string;
+          name: string;
+        };
+        // perform you login logic
+        // find out user from db
+        if (email !== "john@gmail.com" || password !== "12345678") {
+          return null;
+        }
+        // if everything is fine
+        return { id: email, name: email, email: email };
+      },
     }),
   ],
   secret: process.env.JWT_SECRET!,
