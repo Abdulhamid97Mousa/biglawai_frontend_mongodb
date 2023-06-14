4b5458a00ecb36c5ef799a0633c57b8d36674967
Temesgen <tememuruts@gmail.com>
2023-06-14 12:36:16 +0800

    Fix: change in chat\[id] page

--- app/Tryout/chat/[id]/page.tsx
+++ app/Tryout/chat/[id]/page.tsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import Chat from "../../../../components/Chat";
 import ChatInput from "../../../../components/ChatInput";
 import Features from "../../../../components/Features";
-import Select from "react-select";
 
 type Props = {
   params: {
@@ -14,24 +13,30 @@ type Props = {
 function ChatPage({ params: { id } }: Props) {
   const [currentResponse, setCurrentResponse] = useState("");
   const [currentQuestion, setCurrentQuestion] = useState("");
-  const [hidePii, setHidePii] = useState(true)
+  const [hidePii, setHidePii] = useState(true);
   const [lang, setLang] = useState("english");
- 
-  const handlePersonInfo = ()=>{
+
+  const handlePersonInfo = () => {
     setHidePii(!hidePii);
-  }
+  };
 
   return (
     <div className="flex">
       <div className=" flex-1 bg-white ml-10  border-2  border-[#d4d4d4]  h-screen:1000 rounded-md py-10 relative">
         <div className="absolute flex flex-row top-0 right-0  mr-10 mt-3">
-        <button
-          type="button"
-          onClick={handlePersonInfo}
-          className={`p-1 mr-1 rounded-md text-white ${hidePii === true?"bg-blue-700":"bg-red-700"}`}
-        >
-         {hidePii === true? <span>Hide personal information</span> : <span>Show personal information</span>} 
-        </button>
+          <button
+            type="button"
+            onClick={handlePersonInfo}
+            className={`p-1 mr-1 rounded-md text-white ${
+              hidePii === true ? "bg-blue-700" : "bg-red-700"
+            }`}
+          >
+            {hidePii === true ? (
+              <span>Hide personal information</span>
+            ) : (
+              <span>Show personal information</span>
+            )}
+          </button>
           <Features
             placeHolder="Select Language"
             currentLang={lang}
