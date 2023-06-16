d79a4b4c926d77e98bad9eec6d275b4e87c680d8
Abdulhamid97Mousa <80536675+Abdulhamid97Mousa@users.noreply.github.com>
2023-06-16 23:13:08 +0800

    Fix: fixed the chat window size is now fixed and expands properly

--- app/Tryout/chat/[id]/page.tsx
+++ app/Tryout/chat/[id]/page.tsx
@@ -21,14 +21,14 @@ function ChatPage({ params: { id } }: Props) {
   };
 
   return (
-    <div className="flex">
-      <div className=" flex-1 bg-white ml-10  border-2  border-[#d4d4d4]  h-screen:1000 rounded-md py-10 relative">
-        <div className="absolute flex flex-row top-0 right-0  mr-10 mt-3">
+    <div className="flex mt-10">
+      <div className="flex flex-col flex-1 bg-white ml-10 border-2 border-[#d4d4d4] h-screen:1000 rounded-md py-10 relative">
+        <div className="absolute flex flex-row top-0 right-0 mr-10 mt-3">
           <button
             type="button"
             onClick={handlePersonInfo}
-            className={`p-1 mr-1 rounded-md text-white ${
-              hidePii === true ? "bg-blue-700" : "bg-red-700"
+            className={`p-1 mr-5 rounded-md border-[#d4d4d4] border-2 text-black hover:bg-[#0c2474]/50 ${
+              hidePii === true ? "bg-blue-300" : "bg-red-700"
             }`}
           >
             {hidePii === true ? (
@@ -49,15 +49,13 @@ function ChatPage({ params: { id } }: Props) {
           currentResponse={currentResponse}
           currentQuestion={currentQuestion}
         />
-        <div className="flex-1 mr-5 ml-5  ">
-          <ChatInput
-            chatId={id}
-            lang={lang}
-            hidePii={hidePii}
-            setResponse={setCurrentResponse}
-            setCurrentQuestion={setCurrentQuestion}
-          />
-        </div>
+        <ChatInput
+          chatId={id}
+          lang={lang}
+          hidePii={hidePii}
+          setResponse={setCurrentResponse}
+          setCurrentQuestion={setCurrentQuestion}
+        />
       </div>
     </div>
   );
