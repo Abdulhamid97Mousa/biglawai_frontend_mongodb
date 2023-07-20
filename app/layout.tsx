85bfdcb19b550aaf421bf2cf5e002f35be9cb8d5
Abdulhamid97Mousa <80536675+Abdulhamid97Mousa@users.noreply.github.com>
2023-07-20 19:39:35 +0800

    Fix: the layout that is in the root of our application has imports that are not supposed to be used in this layout, but rather in app/Tryout/layout.tsx, these imports are getServerSession and authOptions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,22 @@
 // These styles apply to every route in the application
 import "../styles/globals.css";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../pages/api/auth/[...nextauth]";
 import NavbarLogin from "../components/NavbarLogin";
 import Footer from "../components/Footer";
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession(authOptions);
-
   return (
-    <html lang="en">
-      <body>
-        <NavbarLogin />
-        <div>{children}</div>
-        <Footer />
-      </body>
-    </html>
+    <>
+      <html>
+        <body>
+          {<NavbarLogin />}
+          {children}
+          {<Footer />}
+        </body>
+      </html>
+    </>
   );
 }
