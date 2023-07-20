bae17ccc85a2aca9e5b47426b638242cdf648daa
Abdulhamid97Mousa <80536675+Abdulhamid97Mousa@users.noreply.github.com>
2023-07-20 19:44:58 +0800

    Fix: these are both page components, the one at the root is the landing page, and the other page holds the content of the sidebar, both were optimized

--- app/Tryout/page.tsx
+++ app/Tryout/page.tsx
@@ -9,9 +9,7 @@ const FirstPage = () => {
     <>
       <div className="bg-[#ecf7ff]">
         <div className=" flex flex-col mt-10 items-center justify-center h-screen px-2 text-black ">
-          <div className="text-center justify-center">
-            <button className="text-5xl font-bold ">LAW-AI</button>
-          </div>
+          <div className="text-center justify-center"></div>
         </div>
       </div>
     </>
