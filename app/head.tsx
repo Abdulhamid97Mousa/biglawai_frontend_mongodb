00116cb5b4f20826454d80e575d0f4952e73c003
Abdulhamid97Mousa <80536675+Abdulhamid97Mousa@users.noreply.github.com>
2023-07-20 19:37:19 +0800

    Fix: I have removed head.tsx page components because their content were inaccurate regarding website name and other meta information, I also deleted a function called UploadFile.ts and created another called handleUploadFile.ts, and images/log-2 was causing LPC issue and slowing the website from rendering

--- app/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>LAW-AI</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/images/logo.png" />
-    </>
-  );
-}
