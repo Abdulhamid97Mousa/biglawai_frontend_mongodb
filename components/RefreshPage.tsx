17da67108f387e1b7c8b6dd6946271bd2490ca45
Abdulhamid97Mousa <80536675+Abdulhamid97Mousa@users.noreply.github.com>
2023-07-26 14:11:25 +0800

    Fix: added the env file

---
+++ components/RefreshPage.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+// components/RefreshPage.js
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export function RefreshPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.refresh();
+  }, []);
+
+  return null;
+}
